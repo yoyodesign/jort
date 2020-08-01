d31a6702898a67a05beb5dcbbe417256fa96ebf3
Calvin Davis <hi@calvindavis.co.uk>
2020-08-02 00:58:21 +0100

    test: add unit tests for dummy methods
    re #3

---
+++ src/errorMessages.ts
@@ -0,0 +1,2 @@
+export const IS_GREATER_THAN_ONE_NOT_A_NUMBER =
+	"Value argument must be a number.";
--- src/isGreaterThanOne.ts
+++ src/isGreaterThanOne.ts
@@ -1,3 +1,9 @@
+import { IS_GREATER_THAN_ONE_NOT_A_NUMBER } from "./errorMessages";
+
 export function isGreaterThanOne(value: number): boolean {
+	if (typeof value !== "number" || isNaN(value)) {
+		throw new Error(IS_GREATER_THAN_ONE_NOT_A_NUMBER);
+	}
+
 	return value > 1;
 }
